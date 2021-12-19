ac23a5d496e569ce2b6e97673ce3355d991dde35
ReeyaB123 <reeya4151@gmail.com>
2021-12-19 03:30:57 +0000

    commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,21 @@
+<script>
+    let username = "";
+    let age = "";
+    async function getAge() {
+  fetch('https://api.agify.io/?name=' + username)
+	.then((response) => response.json())
+	.then((data) => {
+  	username = data.name; // Path for temperature
+  	age = data.age; // Path for description
+	});
+}
+</script>
+<h1>Guess Age</h1>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">Name:</label>
+    <input id="username" bind:value={username} type="text" />
+    <button type="submit">Check</button>
+</form>
+
+<p>Age: {age}</p>
